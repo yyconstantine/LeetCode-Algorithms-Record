6f939f90d8e5170c60c46e792ee0d38f761b15e4
yyconstantine <yyconstantine@gmail.com>
2019-11-06 14:33:46 +0800

    add Pascal's Triangle

--- src/array/easy/PascalsTriangle.java
+++ src/array/easy/PascalsTriangle.java
@@ -11,6 +11,10 @@ public class PascalsTriangle {
 
     /**
      * 给定一个非负整数numRows,生成杨辉三角的前numRows行
+     *
+     * 思路：
+     *      每一行根据行数填充对应个数的数据,头尾部写死为1,中间部分取上一次循环结果加和填充
+     *
      * @param numRows 非负整数
      * @return 杨辉三角前numRows行
      */
@@ -20,13 +24,24 @@ public class PascalsTriangle {
         for (int i = 1; i <= numRows; i++) {
             List<Integer> rowList = new ArrayList<>();
             for (int j = 0; j < i; j++) {
-                rowList.add(i);
+                if (i <= 2) {
+                    rowList.add(1);
+                } else {
+                    if (j == 0 || j == i - 1) {
+                        rowList.add(1);
+                    } else {
+                        rowList.add(result.get(i-2).get(j-1)+result.get(i-2).get(j));
+                    }
+                }
             }
-            rowList.add(i);
             result.add(rowList);
         }
 
         return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new PascalsTriangle().generate(5));
     }
 
 
